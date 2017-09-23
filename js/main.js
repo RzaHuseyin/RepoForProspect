e317869c5c3a5024607e08347867996c1564bf6b
RzaHuseyin <rzah1993@gmail.com>
2017-09-23 16:47:28 +0400

    add native chart

--- js/main.js
+++ js/main.js
@@ -17,15 +17,14 @@ window.onload=function(){
 		forTestCanvas(ctx1,canvas1, color1);
 		forTestCanvas(ctx2,canvas2, color2);
 		forTestCanvas(ctx3,canvas3, color3);
+
 		function forTestCanvas(ctx,canvas,color){
 		var W= canvas.width;
 		var H= canvas.height;
 		var degrees =0;
 		var new_degrees=0;
-		var difference= 0 ;
 		var bgcolor= "#d9e4eb"
 		var text;
-		var animation_loop,redraw_loop;
 		// background 
 		function init(ctxcopy,canvascopy){
 			ctx.clearRect(0,0,W,H);
@@ -36,18 +35,18 @@ window.onload=function(){
 			ctx.arc(W/3,H/2,60,0,Math.PI*2,false);
 			ctx.stroke();
 
-
 			var radians = degrees * Math.PI/180;
 			ctx.beginPath();
 			ctx.strokeStyle= color;
 			ctx.lineWidth=10;
 			ctx.arc(W/3,H/2,60,0 - 90*Math.PI/180, radians-90*Math.PI/180,false);
 			ctx.stroke();
-			//Add the text in center 
+			//text of canvas center
 			ctx.fillStyle="#596679";
 			ctx.font="30px arila";
 			text= (Math.floor(degrees/360*100)+3) *20;
-			//    span text edit
+
+			//     push span total text 
 			count++;
 			txt+=text;
 			if (count>2) {
@@ -55,17 +54,17 @@ window.onload=function(){
 				txt=0;
 				count=0;
 			}
+			// for text of canvas center   -if  (text have   3 string). 
+			if(text<1000){
+				text+=" "
+			}
 			//    end span text edit
 			text_width=ctx.measureText(text).width;
 			ctx.fillText(text,W/2.3-text_width,H/2+15); 
 		}
 		function draw(){
 			new_degrees=Math.round(Math.random()*360);
-			difference = new_degrees-degrees;
-			//This will animate the gauge to new positions
-			//The animation will take 1 second
-			//time for each frame is1 second / difference in degrees
-			animation_loop = setInterval(animate_to,10);
+			var animation_loop = setInterval(animate_to,10);
 		}
 		function animate_to(){
 			if(degrees<new_degrees){
@@ -100,96 +99,6 @@ $(document).ready(function(){
 		// $("#left .categoriesList input")[z].attr('checked', true);
 		console.log(z)
 	});
-	
-
-
-
-
-var chart = AmCharts.makeChart( "chartdiv-right", {
-  "type": "serial",
-  "theme": "light",
-  "columnSpacing": 19,
-	"columnSpacing3D": 2,
-	"columnWidth": 0.18,
-	"maxSelectedTime": 5,
-	"minSelectedTime": 7,
-	"startDuration": 1,
-  "dataProvider": [ {
-    "day": "S",
-    "visits": 50
-  }, {
-    "day": "M",
-    "visits": 250
-  }, {
-    "day": "T",
-    "visits": 300
-  }, {
-    "day": "W",
-    "visits": 382
-  }, {
-    "day": "T",
-    "visits": 180
-  }, {
-    "day": "F",
-    "visits": 200
-  }, {
-    "day": "S",
-    "visits": 100
-  }, {
-    "day": "S",
-    "visits": 50
-  }, {
-    "day": "M",
-    "visits": 300
-  }, {
-    "day": "T",
-    "visits": 402
-  }, {
-    "day": "W",
-    "visits": 250
-  }, {
-    "day": "T",
-    "visits": 200
-  },{
-    "day": "F",
-    "visits": 150
-  },{
-    "day": "S",
-    "visits": 100
-  } ],
-  "valueAxes": [ {
-    "gridColor": "#FFFFFF",
-    "gridAlpha": 0.2,
-    "dashLength": 0,
-    "columnSpacing": 3
-  } ],
-  "gridAboveGraphs": true,
-  "startDuration": 1,
-  "graphs": [ {
-    "balloonText": "[[category]]: <b>[[value]]</b>",
-    "fillAlphas": 0.8,
-    "lineAlpha": 0.8,
-    "type": "column",
-    "valueField": "visits"
-  } ],
-  "chartCursor": {
-    "categoryBalloonEnabled": false,
-    "cursorAlpha": 0,
-    "zoomable": true
-  },
-  "categoryField": "day",
-  "categoryAxis": {
-    "gridPosition": "start",
-    "gridAlpha": 0,
-    "tickPosition": "start",
-    "tickLength": 20
-  },
-  "export": {
-    "enabled": true
-  }
-
-} );
-
 
 
 // for list click 
@@ -223,3 +132,35 @@ $( "#fa-angle-down" ).click(function() {
 
 	/* Act on the event */
 });
+
+
+// for catch right
+ var myArry=[150,50,80,38,80,20,10,50,90,150,50,20,150,140]
+    for (var i=0; i<14;i++){
+    			var spinner = document.createElement("div");
+    			spinner.setAttribute('class', 'spinner');
+    			spinner.style.left=(i+1)*25+"px";
+				document.getElementsByClassName("parent")[0].appendChild(spinner);
+    			var spinner2 = document.createElement("div");
+    			spinner2.style.left=(i+1)*25+"px";
+    			spinner2.setAttribute('class', 'spinner2');
+				document.getElementsByClassName("parent")[0].appendChild(spinner2);
+    }
+    			
+    		function first(){
+    			for (var i = 0; i < 14; i++) {
+	    			if(document.getElementsByClassName("spinner2")[i].offsetHeight >myArry[i])
+	    			{
+	    				//clearInterval(t)
+	    			}
+
+	    			else{
+					document.getElementsByClassName("spinner2")[i].style.height=document.getElementsByClassName("spinner2")[i].offsetHeight+1+"px";
+
+	    			}
+    			}
+    			
+    		
+    		}
+    		first();
+    		var t= setInterval(first,10)
